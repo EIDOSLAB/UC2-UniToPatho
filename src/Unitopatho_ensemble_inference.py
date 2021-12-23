98f5ca809aa9105097282e60bbaeb450090c8b8f
PerloDaniele <perlo.daniele@gmail.com>
2021-12-23 14:02:13 +0100

    Update Unitopatho_ensemble_inference.py

--- src/Unitopatho_ensemble_inference.py
+++ src/Unitopatho_ensemble_inference.py
@@ -19,7 +19,7 @@
 # SOFTWARE.
 
 """\
-colorectal polyp classification inference for UniToPatho.
+colorectal polyp classification inference example.
 """
 
 import argparse
@@ -28,315 +28,119 @@ import numpy as np
 import os
 import pyecvl.ecvl as ecvl
 import pyeddl.eddl as eddl
-from pyeddl.tensor import Tensor, DEV_CPU
-import pandas as pd
-import models
+from pyeddl.tensor import Tensor
+
+from models import resnet18, Resnet18_onnx, Resnet50_onnx
 import time
-from utils import comp_stats, gen_sub_crops_tensors, SLIDE_RES ,get_sub_crops_tensors, gen_sub_crops
-from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
-import time
-from tqdm import tqdm
 import utils
-import wandb
-
-
-HP, NORM, TAHG, TALG, TVAHG, TVALG = 0, 1, 2, 3, 4, 5
-NOT_ADEN, TA, TVA = 0, 2, 3
-
-MEM_CHOICES = ("low_mem", "mid_mem", "full_mem")
+from sklearn.metrics import confusion_matrix
+import time
 
 def main(args):
-
-    if args.wandb:
-        config = dict (
-            batch_size = args.batch_size,
-            architecture = "resnet18",
-            dataset_id = "unitopato",
-            infra = 'HPC4AI',
-        )
-
-        run = wandb.init(
-           name=args.name,
-           project="uc2-eddl",
-           notes="icip-inference-eddl",
-           tags=["icip"],
-           config=config,
-        )
-
-    args.use_temp = args.temp_folder is not None and args.temp_folder != ''
-    start_time = time.time()
+    num_classes = 6
+    size = [args.input_size, args.input_size]  # size of images
     
+    # ECVL works in BGR
+    mean = utils.adenoma_mean[[2,1,0]]
+    std = utils.adenoma_mean[[2,1,0]]
 
-    """
-    ############################################################################
-    ######################### ADENOMA PREDICTION ###############################
-    ############################################################################
-    """
-    
-    print('\n-------------------- ADENOMA PREDICTION --------------------')
-    start_time_adenoma = time.time()
-    image_size = 224
-    size = [image_size, image_size]
-    adenoma_mean = utils.adenoma_mean[None,:,None,None]
-    adenoma_std = utils.adenoma_std[None,:,None,None]
+    in_ = eddl.Input([3, size[0], size[1]])
 
-    ## Set Adenoma Type preditcion test set image transformations
+    if args.pretrain == -1:
+        out = resnet18(in_, num_classes)
+        net = eddl.Model([in_], [out])
+    elif args.pretrain == 50:
+        net, out = Resnet50_onnx(pretreined=True)
+    elif args.pretrain == 18:
+        net, out = Resnet18_onnx(pretreined=True)
+    else: 
+        net, out = Resnet50_onnx(pretreined=False)
+
+    eddl.build(
+        net,
+        eddl.sgd(0.001, 0.9),
+        ["soft_cross_entropy"],
+        ["categorical_accuracy"],
+        eddl.CS_GPU(args.gpu) if args.gpu else eddl.CS_CPU(),
+        init_weights = args.pretrain == -1
+    )
+    eddl.summary(net)
+
     test_augs = ecvl.SequentialAugmentationContainer([
         ecvl.AugResizeDim(size),
-        # Normalization done manually on the Tensor
-        #ecvl.AugToFloat32(),
-        #ecvl.AugNormalize(utils.adenoma_mean[[2,1,0]], utils.adenoma_mean[[2,1,0]])
+        ecvl.AugToFloat32(),
+        ecvl.AugNormalize(mean, std)
     ])
+    dataset_augs = ecvl.DatasetAugmentations([test_augs, test_augs, test_augs])
 
-    dataset_augs = ecvl.DatasetAugmentations([None, None, test_augs])
-
-    ## Loading Adenoma Type preditcion test set
+    #already contains proper test set
     print("Reading dataset")
-    ds_file = os.path.join(args.in_ds,'7000_224','deephealth-uc2-7000_224.yml')
+    ds_file = os.path.join(args.in_ds,args.yml_name)
     if not os.path.isfile(ds_file):
         raise Exception('missing Dataset yaml file')
+    
     d = ecvl.DLDataset(ds_file, args.batch_size, dataset_augs)
-    print('Classes: ',d.classes_)
-    d.SetSplit(ecvl.SplitType.test)
-    num_samples = len(d.GetSplit())
-    num_batches = num_samples // args.batch_size
 
-    ## Enseble 3 models predictions for Adenoma
-    preds_adenoma, gt_adenoma = [],[]
     x = Tensor([args.batch_size, d.n_channels_, size[0], size[1]])
     y = Tensor([args.batch_size, len(d.classes_)])
-    
+    d.SetSplit(ecvl.SplitType.training)
+    num_samples = len(d.GetSplit())
 
-    for classifier_idx in [0,1,2]:
-        print('\n-----------------------------')
-        print(f'Running classifier {classifier_idx+1} of 3...')
-        # Loading model
-        net, out = models.load_onnx_models(f'adenoma_classifier.3c.{classifier_idx}')
-        eddl.build(
-            net, eddl.sgd(0.001, 0.9),["soft_cross_entropy"], ["categorical_accuracy"],
-            eddl.CS_GPU(args.gpu,args.lsb,mem=args.mem) if args.gpu else eddl.CS_CPU(), init_weights=False
-        )
-        # Normalization done manually on the Tensor
-        adenoma_mean = Tensor.fromarray(np.full((args.batch_size, d.n_channels_, size[0], size[1]), adenoma_mean), dev=net.dev)
-        adenoma_std = Tensor.fromarray(np.full((args.batch_size, d.n_channels_, size[0], size[1]), adenoma_std), dev=net.dev)
+    d.SetSplit(ecvl.SplitType.test)
+    num_samples = len(d.GetSplit())
+    print(f'Testing {num_samples} images')
+    num_batches = num_samples // args.batch_size
+    eddl.set_mode(net, 0)
 
-        eddl.set_mode(net, 0)
-        d.ResetAllBatches()
+    if not os.path.exists(args.ckpts):
+        raise RuntimeError('Checkpoint "{}" not found'.format(args.ckpts))
+    eddl.load(net, args.ckpts, "bin")
 
-        # inference loop
-        print('Inference Adenoma Type')
-        preds = []
-        for s in tqdm(range(num_batches)):
-            d.LoadBatch(x, y)
+    print("Testing")
+    start_time = time.time()
+    np_out,np_out = None, None
+    for b in range(num_batches):
 
-            # !RGB model: (note ECVL works in BGR, so we have to change the input to feed network trained on pytorch)
-            x = Tensor.fromarray(x.getdata()[:,[2,1,0]], dev=net.dev)
-            x.sub_(adenoma_mean)
-            x.div_(adenoma_std)
- 
-            eddl.forward(net, [x])
-            output = eddl.getOutput(out)
-            preds.append(output.getdata())
+        print("Batch {:d}/{:d}".format(b + 1, num_batches))
+        d.LoadBatch(x, y)
+        eddl.forward(net, [x])
+        output = eddl.getOutput(out)
 
-        preds_adenoma.append(np.stack(preds,axis=0))
-      
-    ## ensemble results
-    preds_adenoma_sfm = np.squeeze(np.mean(preds_adenoma, axis=0))
-    preds_adenoma = np.argmax(preds_adenoma_sfm, axis=1)
+        if np_out is None:
+            np_out = np.argmax(output.getdata(),axis=1)
+            np_y = np.argmax(y.getdata(),axis=1)
+        else:
+            np_out, np_y = np.concatenate((np_out, np.argmax(output.getdata(),axis=1)), axis=0),  np.concatenate((np_y, np.argmax(y.getdata(),axis=1)), axis=0)
 
-
-    preds_adenoma[preds_adenoma == 2] = TVA
-    preds_adenoma[preds_adenoma == 1] = TA
-    preds_adenoma[preds_adenoma == 0] = NOT_ADEN
-
-    adenomas = (preds_adenoma == TA) | (preds_adenoma == TVA)
-    data = {
-        'predicted_adenoma': adenomas.astype(np.int),
-        'predicted_type': preds_adenoma
-    }
-    adenoma_df = pd.DataFrame(data)
-    adenoma_df.to_csv('predictions_adenoma.csv', index=False)
-    print("---Time to Inference Adenoma - UnitoPatho: %s seconds ---" % (time.time() - start_time_adenoma))
-    
-    
-    """
-    ############################################################################
-    ########################### GRADE PREDICTION ###############################
-    ############################################################################
-    """
-    
-    print('\n-------------------- GRADE PREDICTION ----------------------')
-    start_time_grade = time.time()
-    
-    image_size, centersize, cropsize = int( 800/ SLIDE_RES ), 2000, 800 # changes to these values make temp_folder obsolete
-    size = [image_size, image_size]
-    max_num = ((centersize//(cropsize//2))-1) ** 2 #step == 2
-    grade_mean = utils.grade_mean[None,:,None,None]
-    grade_std = utils.grade_std[None,:,None,None]
-
-    ## Loading Grade Type preditcion test set
-    print("Reading dataset")
-    ds_file = os.path.join(args.in_ds,'7000','deephealth-uc2-7000.yml')
-    if not os.path.isfile(ds_file):
-        raise Exception('missing Dataset yaml file')
-    d = ecvl.Dataset(ds_file)
-    print('Classes: ',d.classes_)
-
-    samples = [ s.location_[0] for s in np.array(d.samples_)[d.split_.test_] ]
-    #samples = [ s for s in samples if '164-' in s]
-
-    if args.use_temp:
-        print('Create Temp folder')
-        gen_sub_crops( samples , os.path.join(args.temp_folder,'grade'), size = cropsize, um = centersize )
-    
-    num_samples = len(samples)
-
-    ## Enseble 3 models predictions for Grade Type
-    preds_grade = []
-    for classifier_idx in [0,1,2]: 
-        print('\n-----------------------------')
-        print(f'Running classifier {classifier_idx+1} of 3...')
-        # Loading model
-        net, out = models.load_onnx_models(f'grade_classifier.800.{classifier_idx}')
-        eddl.build(
-            net, eddl.sgd(0.001, 0.9),["soft_cross_entropy"], ["categorical_accuracy"],
-            eddl.CS_GPU(args.gpu,args.lsb,mem=args.mem) if args.gpu else eddl.CS_CPU(), init_weights=False
-        )
-        eddl.set_mode(net, 0)
-
-        # Normalization done manually on the Tensor
-        grade_mean = Tensor.fromarray(np.full((max_num, 3, size[0], size[1]), grade_mean), dev=net.dev)
-        grade_std = Tensor.fromarray(np.full((max_num, 3, size[0], size[1]), grade_std), dev=net.dev)
-
-        # inference 1 sample each pass: we need to process images full-resolution subcrops
-        preds=[]
-        print('Inference Grade Type')
-        for j,sample in enumerate(tqdm(samples)):
-            #load subcrops at 800 micron for each 7000 micron sample
-
-            if args.use_temp:
-                batchLoader = get_sub_crops_tensors( sample , os.path.join(args.temp_folder,'grade'), grade_mean, grade_std, batch_size = args.fullres_batch_size , px_size = image_size, gpu = net.dev)
-            else:
-                batchLoader = gen_sub_crops_tensors( sample, grade_mean, grade_std, batch_size = args.fullres_batch_size , size = cropsize, um = centersize, px_size = image_size, gpu = net.dev)
-
-            #initialize prediction to fixed dimension to the max number of subcrops
-            s_preds = np.empty((max_num,2))
-            s_preds.fill(np.nan)
+    scores = utils.comp_stats( confusion_matrix(np_y,np_out) )
             
-            for i,x in enumerate(batchLoader):
+    weights = 1. / np.unique(np_y, return_counts=True)[1]
+    weights = [ w / weights.sum() for w in weights]
 
-                eddl.forward(net, [x])
-                output = eddl.getOutput(out)
-                idx = i*args.fullres_batch_size
-                s_preds[idx:idx+x.shape[0]] = output.getdata()
+    acc_score = (scores['ACC']*weights).sum()
+    ba_score = (scores['BA']*weights).sum()
+    f1_score = (scores['F1']*weights).sum()
+    sens_score = (scores['TPR']*weights).sum()
+    spec_score = (scores['TNR']*weights).sum()
+    precision_score = (scores['PPV']*weights).sum()
 
-            preds.append(np.stack(s_preds,axis=0))
+    print("F1 Score:\t", f1_score)
+    print("Recall/Sensitivity Score:\t", sens_score)
+    print("Specificity Score:\t", spec_score)
+    print("Precision Score:\t", precision_score)
+    print("Balanced accuracy:\t", ba_score)
+    print("Categorical accuracy:\t", acc_score)
 
-        preds_grade.append(np.stack(preds,axis=0))
-        
-    ## ensemble results 
-    preds_grade_sfm = np.squeeze(np.mean(preds_grade, axis=0))
 
-    ## takes note of HG predictions for each subcrop
-    preds_grade_sfm = np.squeeze(preds_grade_sfm[:,:,1])
+    print("---Time to Inference:\t%s seconds ---" % (time.time() - start_time))
 
-    grade_df = pd.DataFrame(np.squeeze(preds_grade_sfm))
-    grade_df.to_csv('predictions_grade.csv', index=False)
-    print("---Time to Inference Grade - UnitoPatho: %s seconds ---" % (time.time() - start_time_grade))
-
-    """
-    ############################################################################
-    ############################# HP PREDICTION ################################
-    ############################################################################
-    """
-    print('\n-------------------- HP PREDICTION -------------------------')
-    start_time_hp = time.time()
-    
-    image_size, centersize, cropsize = int( 800/ SLIDE_RES ), 1500, 800 # changes to these values make temp_folder obsolete
-    size = [image_size, image_size]
-    max_num = ((centersize//(cropsize//2))-1) ** 2 #step == 2 
-    HP_mean = utils.HP_mean[None,:,None,None]
-    HP_std = utils.HP_std[None,:,None,None]
-
-    ## Loading Hyperplastic preditcion test set
-    print("Reading dataset")
-    ds_file = os.path.join(args.in_ds,'7000','deephealth-uc2-7000.yml')
-    if not os.path.isfile(ds_file):
-        raise Exception('missing Dataset yaml file')
-    d = ecvl.Dataset(ds_file)
-    print('Classes: ',d.classes_)
-
-    samples = [ s.location_[0] for s in np.array(d.samples_)[d.split_.test_] ]
-
-    if args.use_temp:
-        print('Create Temp folder')
-        gen_sub_crops( samples , os.path.join(args.temp_folder,'hp'), size = cropsize, um = centersize )
-    
-    num_samples = len(samples)
-        
-    ## Enseble 3 models predictions for Hyperplastic
-    preds_hp = []
-    
-    for classifier_idx in [0,1,2]: 
-        print('\n-----------------------------')
-        print(f'Running classifier {classifier_idx+1} of 3...')
-        # Loading model
-        net, out = models.load_onnx_models(f'hp_classifier.800.{classifier_idx}')
-        eddl.build(
-            net, eddl.sgd(0.001, 0.9),["soft_cross_entropy"], ["categorical_accuracy"],
-            eddl.CS_GPU(args.gpu,args.lsb,mem=args.mem) if args.gpu else eddl.CS_CPU(), init_weights=False
-        )
-        eddl.set_mode(net, 0)
-        # Normalization done manually on the Tensor
-        HP_mean = Tensor.fromarray(np.full((max_num, 3, size[0], size[1]), HP_mean), dev=net.dev)
-        HP_std = Tensor.fromarray(np.full((max_num, 3, size[0], size[1]), HP_std), dev=net.dev)
-
-        # inference 1 sample each pass: we need to process images full-resolution subcrops
-        preds=[]
-        print('Inference Hyperplastic')
-        for j,sample in enumerate(tqdm(samples)):
-
-            #load subcrops at 800 micron for each 7000 micron sample
-            if args.use_temp:
-                batchLoader = get_sub_crops_tensors( sample , os.path.join(args.temp_folder,'hp'), HP_mean, HP_std, batch_size = args.fullres_batch_size , px_size = image_size, um = centersize, gpu = net.dev)
-            else:
-                batchLoader = gen_sub_crops_tensors( sample, HP_mean, HP_std, batch_size = args.fullres_batch_size , size = cropsize, um = centersize, px_size = image_size, gpu = net.dev)
-            
-            s_preds = []
-            for i,x in enumerate(batchLoader):
-
-                eddl.forward(net, [x])
-                output = eddl.getOutput(out)
-                s_preds.append(output.getdata())
-            
-            # sample mean prediction
-            if len(s_preds) > 0 :
-                s_preds = np.stack(s_preds, axis=0)
-                s_preds = np.mean(s_preds, axis=1)
-            else: #no tissue subcrops 
-                s_preds = np.zeros((1,6))
-            preds.append(s_preds)
-
-        preds_hp.append(np.stack(preds,axis=0))
-        
-    ## ensemble results 
-    preds_hp_sfm = np.squeeze(np.mean(preds_hp, axis=0))
-    preds_hp = np.argmax(preds_hp_sfm, axis=1)
-
-    hp_df = pd.DataFrame(preds_hp)
-    hp_df.to_csv('predictions_hp.csv', index=False)
-    print("---Time to Inference HP - UnitoPatho: %s seconds ---" % (time.time() - start_time_hp))
-    print("---Time to Slide Inference - UnitoPatho: %s seconds ---" % (time.time() - start_time))
-    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("in_ds", help = 'path to UnitoPatho', metavar="INPUT_DATASET", default='/mnt/data/unitopath-public')
-    parser.add_argument("--batch-size", help="batch-size for 224x224 resolution images", type=int, metavar="INT", default=248)
-    parser.add_argument("--fullres-batch-size", help="batch-size for full resolution images", type=int, metavar="INT", default=6)
-    parser.add_argument('--gpu', nargs='+', type=int, required=False, help='`--gpu 1 1` to use two GPUs', default=1)
-    parser.add_argument("--temp_folder", help="temporary folder for inference speedup (slow down the first run, high storage demand )", default='')
-    parser.add_argument('--wandb', action='store_true', help='enable wandb logs', default=True)
-    parser.add_argument("--name", help='run name', type=str,  default='icip_inference_eddl')
-    parser.add_argument("--mem", metavar="|".join(MEM_CHOICES), choices=MEM_CHOICES, default="full_mem")
-    parser.add_argument("--lsb", help='multi-gpu update frequency', type=int, metavar="INT", default=1)
+    parser.add_argument("in_ds", help = 'path to the dataset', metavar="INPUT_DATASET")
+    parser.add_argument("--ckpts", help='checkpoint path', metavar='CHECKPOINTS_PATH', default='checkpoints/cpc_classification_checkpoint_epoch_4.bin')
+    parser.add_argument("--batch-size", help='batch-size', type=int, metavar="INT", default=218)
+    parser.add_argument('--gpu', nargs='+', type=int, required=False, help='`--gpu 1 1` to use two GPUs')
+    parser.add_argument("--pretrain", help='use pretrained resnet network: default=18, allows 50 and -1 (resnet 18 not pretrained)', type=int,  default=18)
+    parser.add_argument("--yml-name", help='yml name (default=deephealth-uc2-800_224_balanced.yml )', type=str, default='deephealth-uc2-800_224_balanced.yml')
+    parser.add_argument("--input-size", type=int, help='224 px or original size (1812 at 800um)', default=224)
     main(parser.parse_args())
